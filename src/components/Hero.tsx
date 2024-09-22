f7634cfc5261c9d096dffcbf1b340751e79aea83
syahrul jay <syahruljaylani73@gmail.com>
2024-09-22 19:18:14 +0700

    fixing hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,13 +7,13 @@ const Hero = () => {
   return (
     <MaxWidthWrapper classname="mt-12 md:-mt-12 ">
       <div className="flex md:flex-row flex-col w-full bg-white">
-        <div className="flex flex-col  w-full">
+        <div className="flex flex-col w-full">
           <div className="border-2 border-primary rounded-full px-3 py-1 w-fit">
             <li className="text-primary text-center">Memberdayakan Pertanian Modern</li>
           </div>
           <h1 className="text-5xl lg:text-6xl font-bold text-primary mt-4">Solusi pertanian berkelanjutan untuk masa depan yang lebih baik.</h1>
 
-          <p className="text-primary mt-6">Menghadirkan teknologi mutakhir dan praktik inovatif ke dunia pertanian, membantu Anda memaksimalkan hasil, mengurangi limbah, dan meningkatkan keberlanjutan</p>
+          <p className="text-primary mt-6 w-3/4 tracking-tight">Menghadirkan teknologi mutakhir dan praktik inovatif ke dunia pertanian, membantu Anda memaksimalkan hasil, mengurangi limbah, dan meningkatkan keberlanjutan</p>
 
           <div className="flex gap-4 items-center mt-6 lg:mt-8">
             <a href="#" className="flex gap-3 px-4 py-2 rounded-full justify-center w-fit items-center bg-primary hover:bg-primaryHover transition-all">
@@ -28,7 +28,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="grid w-full grid-cols-2 gap-4 mt-6">
+        <div className="grid w-full grid-cols-2 gap-4 mt-6 md:mt-0">
           <div className="col-span-2 relative w-full h-[250px]  md:h-[300px] rounded-3xl overflow-hidden">
             <Image alt="image" src="/hero1.jpg" fill className="w-full h-full object-cover" />
           </div>
